af18520c838eaec7d0581904d16c96f78e49937f
Guilherme <guilhermegpereira26@hotmail.com>
2025-06-30 06:27:34 -0300

    Add controller package and AuthorController

---
+++ src/main/java/io/github/Guimaraes131/libraryapi/controller/AuthorController.java
@@ -0,0 +1,11 @@
+package io.github.Guimaraes131.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/author")
+public class AuthorController {
+
+
+}
