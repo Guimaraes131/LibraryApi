d9753fb2f555c3d4042d56c911c553e0b120b3c5
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-16 06:13:54 -0300

    feat: add getmapping to /

--- src/main/java/io/github/Guimaraes131/libraryapi/controller/LoginController.java
+++ src/main/java/io/github/Guimaraes131/libraryapi/controller/LoginController.java
@@ -1,7 +1,9 @@
 package io.github.Guimaraes131.libraryapi.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -9,5 +11,11 @@ public class LoginController {
     @GetMapping("/login")
     public String loginPage() {
         return "login";
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String homePage(Authentication authentication) {
+        return "Hello " + authentication.getName();
     }
 }
